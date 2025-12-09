a143b04dd5f8918fcb64d957669f082570faba7a
krishnasagitario-dot <krishna.sagitario@gmail.com>
2025-12-09 16:54:16 -0300

    Delete appProyecto1 directory

--- appProyecto1/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class Appproyecto1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appProyecto1'
-
-    def ready(self):
-        import appProyecto1.signals
